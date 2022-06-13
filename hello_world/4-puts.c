7a38699754297aa108df40e4e6cdb6e34a182c5f
yenniferm <jemp1886@hotmail.com>
2022-06-13 16:48:45 -0500

    push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
  
   // initializing strings
-  char str1[] = ""Programming is like building a multilingual puzzle";
-  char str2[] = "Using puts in C";
-
+  char str1[] = "\"Programming is like building a multilingual puzzle";
+ 
   // writing to stdout
   puts(str1);
-  puts(str2);
-
+ 
   return 0;
 }
