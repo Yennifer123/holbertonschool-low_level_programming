ecd76042a6247d742d84086bb8ce73093b55d18d
yenniferm <jemp1886@hotmail.com>
2022-06-13 18:35:02 -0500

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-prinf("Size of a char: 1 byte(s)");
+printf("Size of a char: 1 byte(s)");
 printf("Size of an int: 4 byte(s)");
 printf("Size of a long int: 4 byte(s)");
 printf("Size of a long long int: 8 byte(s)");
