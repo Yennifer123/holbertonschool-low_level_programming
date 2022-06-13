fa30babfa6b2e629bca5e075501d337823317539
yenniferm <jemp1886@hotmail.com>
2022-06-13 16:57:50 -0500

    push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,11 +4,8 @@
  * Use the function puts
  */
 int main(void)
-{
- 
-  char str1[] = "\"Programming is like building a multilingual puzzle";
- 
-  puts(str1);
- 
-  return 0;
+{ 
+char str1[] = "\"Programming is like building a multilingual puzzle";
+puts(str1);
+return 0;
 }
