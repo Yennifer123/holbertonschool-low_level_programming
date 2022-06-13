0690d2259fcf5c504a72490a63c73ab3d3effc7e
yenniferm <jemp1886@hotmail.com>
2022-06-13 18:33:14 -0500

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints the size of various types 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+prinf("Size of a char: 1 byte(s)");
+printf("Size of an int: 4 byte(s)");
+printf("Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+return (0);
+}
