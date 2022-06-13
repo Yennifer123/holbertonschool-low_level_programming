3bc8e35cfc32ceb4fad7211a47ac23e596238cbd
yenniferm <jemp1886@hotmail.com>
2022-06-13 18:42:11 -0500

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)");
-printf("Size of an int: 4 byte(s)");
-printf("Size of a long int: 4 byte(s)");
-printf("Size of a long long int: 8 byte(s)");
-printf("Size of a float: 4 byte(s)");
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
 }
