d0c117df97b4d5114649153fc078b365e8f03783
yenniferm <jemp1886@hotmail.com>
2022-06-13 15:09:33 -0500

    hello, puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+ 
+  // initializing strings
+  char str1[] = ""Programming is like building a multilingual puzzle";
+  char str2[] = "Using puts in C";
+
+  // writing to stdout
+  puts(str1);
+  puts(str2);
+
+  return 0;
+}
