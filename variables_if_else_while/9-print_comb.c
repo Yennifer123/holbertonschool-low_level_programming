1a2feff9c96033df8a986806a3b14bec0faea3e2
yenniferm <jemp1886@hotmail.com>
2022-06-14 23:20:59 -0500

    prints all possible combinations of single-digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar((number % 10) + '0');
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
+
