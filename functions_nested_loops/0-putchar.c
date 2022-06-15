13f5551d825a9a158f3db6459357fd1933d65503
yenniferm <jemp1886@hotmail.com>
2022-06-15 15:36:02 -0500

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: O
- * 
- */
+* main - _putchar - writes in program
+* Return: 0
+*/
 int main(void)
 {
- _putchar(95);
- _putchar(112);
- _putchar(117);
- _putchar(116);
- _putchar(99);
- _putchar(104);
- _putchar(97);
- _putchar(114);
- _putchar('\n');
- return (0);
- } 
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+return (0);
+}
+
