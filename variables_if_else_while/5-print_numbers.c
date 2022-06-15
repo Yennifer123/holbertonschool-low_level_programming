72aac6fe33db569964c0b69f6ad3d654a4d11ad6
yenniferm <jemp1886@hotmail.com>
2022-06-14 22:38:24 -0500

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d",number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
