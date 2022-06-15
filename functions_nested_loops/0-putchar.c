6bc9330e4b266ea8d63a8fdbbaa378a8185a8cbf
yenniferm <jemp1886@hotmail.com>
2022-06-15 11:32:10 -0500

    C - Functions, nested loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - using ascii
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+char str1[] = '95'putchar;
+puts(str1);
+return (0);
+}
