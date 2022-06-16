6e6fb5dc72367efffa2d014e2baaa73bc1e627bc
yenniferm <jemp1886@hotmail.com>
2022-06-15 22:28:15 -0500

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs -- get the abs value of number
+ *@n number: to compare
+ *Return: of a number
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
