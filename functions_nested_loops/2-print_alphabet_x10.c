7a1edfcf24905c944e127cc9296a1a6f52ba0268
yenniferm <jemp1886@hotmail.com>
2022-06-15 20:45:19 -0500

    void print_alphabet_x10(void)

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 --  10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int times = 0;
+
+	while (times < 10)
+{
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	times++;
+	}
+	_putchar('\n');
+}
+
