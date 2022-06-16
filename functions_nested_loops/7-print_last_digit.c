9a82ec15220186ce1975fab29ecb6353ada4ef7d
yenniferm <jemp1886@hotmail.com>
2022-06-15 22:51:08 -0500

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_last_digit --  checks of the last digit
+ *@n: number to get the last digit
+ *Return: last digit of the number
+ */
+int print_last_digit(int n)
+{
+int lastDigit = 0;
+if (n < 0)
+{
+n = n * -1;
+}
+lastDigit = n % 10;
+_putchar('0' + lastDigit);
+return (lastDigit);
+}
