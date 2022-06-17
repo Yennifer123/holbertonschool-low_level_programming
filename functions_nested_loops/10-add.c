ba3d865df8143fc5bd2f0b39a913a003ea6c2f3f
yenniferm <jemp1886@hotmail.com>
2022-06-16 21:25:47 -0500

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add -- numberOne numberTwo
+ *@numberOne: first number
+ *@numberTwo: second number
+ *Return: result
+ */
+int add(int numberOne, int numberTwo)
+{
+int result = (numberOne + numberTwo);
+return (result);
+}
+
