3c72b5c4035aec73164c08151978e742a4a17360
yenniferm <jemp1886@hotmail.com>
2022-06-20 14:40:05 -0500

    1-isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ *_isdigit -- checks for uppercase character.
+ *@c: represents the digit
+ *Return: 1 digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+int result = 0;
+if (c >= '0'  && c <= '9')
+{
+result = 1;
+}
+return (result);
+}
 
