5b80e34ec4fc4db3fa6f2405ef06fe477a8050be
yenniferm <jemp1886@hotmail.com>
2022-06-20 20:25:10 -0500

    moreNumbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *more_numbers -- 10 times the numbers, from 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+int number;
+int times;
+for (times = 0; times < 9; times++)
+{
+for (number = 0; number <= 14; number++)
+{
+if (number > 9)
+{
+_putchar(number / 10 + '0');
+}
+_putchar(number % 10 + '0');
+}
+_putchar('\n');
+}
+}
+
